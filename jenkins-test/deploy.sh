677e4097d684c65f90114f975f1e7c5b8d9c7718
wangwei007 <784264288@qq.com>
2018-11-02 07:33:06 +0800

    修改maven打包的项目名称

--- jenkins-test/deploy.sh
+++ jenkins-test/deploy.sh
@@ -29,13 +29,13 @@ killTomcat
 # 删除原有工程
 rm -rf $TOMCAT_APP_PATH/webapps/ROOT
 rm -f $TOMCAT_APP_PATH/webapps/ROOT.war
-rm -f $TOMCAT_APP_PATH/webapps/jenkins-test.war
+rm -f $TOMCAT_APP_PATH/webapps/jenkins-test-0.0.1-SNAPSHOT.war
 
 # 复制新的工程
-cp $PROJ_PATH/jenkins-test/target/jenkins-test.war $TOMCAT_APP_PATH/webapps/
+cp $PROJ_PATH/jenkins-test/target/jenkins-test-0.0.1-SNAPSHOT.war $TOMCAT_APP_PATH/webapps/
 
 cd $TOMCAT_APP_PATH/webapps/
-mv jenkins-test.war ROOT.war
+mv jenkins-test-0.0.1-SNAPSHOT.war ROOT.war
 
 # 启动Tomcat
 cd $TOMCAT_APP_PATH/
